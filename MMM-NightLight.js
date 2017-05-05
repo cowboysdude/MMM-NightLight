83250d43714618b987c4a203f3453454a4f90c7c
John Wade <cowboysdude@stny.rr.com>
2017-05-05 19:21:24 -0400

    Add files via upload

---
+++ MMM-NightLight.js
@@ -0,0 +1,47 @@
+ /* Magic Mirror
+  * Module: MMM-M
+  *
+  * By John Wade
+  * MIT Licensed.
+  */
+ Module.register("MMM-NightLight", {
+
+     // Module config defaults.
+     defaults: {
+         //color: #fff
+     },
+
+     // Define required scripts.
+     getScripts: function() {
+         return ["moment.js"];
+     },
+
+     getStyles: function() {
+         return ["MMM-NightLight.css"];
+     },
+
+     // Define start sequence.
+     start: function() {
+         Log.info("Starting module: " + this.name);
+
+         // Set locale.
+         moment.locale(config.language);
+         this.today = "";
+         this.loaded = true;
+     },
+
+     getDom: function() {
+     	
+         var wrapper = document.createElement("div");
+         wrapper.classList.add("veil");
+         wrapper.style.maxWidth = this.config.maxWidth;
+
+         var NightLight = document.createElement("div");
+         NightLight.classList.add("pole");
+         NightLight.innerHTML = '';
+         wrapper.appendChild(NightLight);
+				 
+         return wrapper;
+     },
+
+ });
